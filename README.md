914bef62616f66f9a81cfbcd761e6fe7325cf6c7
Askild Olsen <askild@xangeli.com>
2022-01-02 14:29:16 +0100

    Drupal docker, update and deploy

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # dataplattform
 Digitalisert dataplattform
 
-dotnet build
 docker-compose up -d --build
 
-docker exec -it dataplattform_drupal_1 drush -y site:install standard --account-pass=***
+docker-compose exec drupal drush user:login --uri=http://localhost
+
+docker-compose down --remove-orphans -v
---
+++ drupal/post-start-drupal.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Post startup"
+
+until [ $(curl -fLSs http://127.0.0.1 -o /dev/null -w '%{http_code}\n') -eq "200" ] ; do sleep 5; done;
+
+BOOTSTRAP=$(drush core:status --format=string bootstrap)
+
+if [ "$BOOTSTRAP" = 'Successful' ]
+then
+  echo "Initializing already complete"
+else
+  echo "Initializing with drush"
+  drush -y site:install minimal 2>&1 | sed 's/password: .*/password: *****/'
+  drush theme:enable gin
+  drush pm:enable config
+  drush -y config:set system.theme default gin
+  drush -y config:set system.theme admin ''
+  drush theme:uninstall stark
+  echo "Initializing complete"
+fi
+
+drush -y config:import --partial
+drush cache:rebuild

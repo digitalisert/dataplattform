93ea2ad82b0948d4dc9fd0546fa9b6c6337e284a
Askild Olsen <askild@xangeli.com>
2022-01-29 16:24:26 +0100

    Ekstrakt og last for Webforms
    Hver submission blir en egen .json i en .zip-fil
    som lasteren itererer over.
    
    Foreløpig bare om den heter "test". Må bli dynamisk...

--- drupal/export.sh
+++ drupal/export.sh
@@ -2,3 +2,5 @@
 
 drush views:execute resource rest_export_1 > /opt/drupal/export/resource.json
 drush views:execute property rest_export_1 > /opt/drupal/export/property.json
+
+drush webform:export --exporter=json --archive-type=zip --destination=/opt/drupal/export/webform-test.zip test

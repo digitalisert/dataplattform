9d4a18e55be9b59fac40a67c0ba3a9ed7b6f6720
Askild Olsen <askild@xangeli.com>
2022-02-08 22:36:15 +0100

    Eksporter kun webforms referert fra Webform Node

--- drupal/export.sh
+++ drupal/export.sh
@@ -2,5 +2,8 @@
 
 drush views:execute resource rest_export_1 > /opt/drupal/export/resource.json
 drush views:execute property rest_export_1 > /opt/drupal/export/property.json
+drush views:execute webform rest_export_1 > /opt/drupal/export/webform.json
 
-drush webform:export --exporter=json --archive-type=zip --destination=/opt/drupal/export/webform-test.zip test
+for webform in $(jq -r '.[].webform[].target_id' /opt/drupal/export/webform.json); do
+  drush webform:export --exporter=json --archive-type=zip --destination=/opt/drupal/export/webform-${webform}.zip ${webform}
+done
